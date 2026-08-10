c180e51a0912bd1240c6a4dd537060cdbc9ffb4c
julitobalagbagjr <302093482+julitobalagbagjr@users.noreply.github.com>
2026-08-10 08:36:14 +0800

    Task10

---
+++ Tasks/Task10.java
@@ -0,0 +1,29 @@
+public class Task10 {
+
+    static class Student {
+        private String firstName;
+        private String lastName;
+
+        public Student(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public void printFullName() {
+            System.out.println(this.firstName + " " + this.lastName);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Student[] students = new Student[] {
+            new Student("Morgan", "Freeman"),
+            new Student("Brad", "Pitt"),
+            new Student("Kevin", "Spacey")
+        };
+
+        for (Student student : students) {
+            student.printFullName();
+        }
+    }
+}
