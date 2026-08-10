23b02028097a97e0f38950059b98b30b232ebcbf
julitobalagbagjr <302093482+julitobalagbagjr@users.noreply.github.com>
2026-08-10 09:06:22 +0800

    Task2

---
+++ Tasks/Task2.java
@@ -0,0 +1,15 @@
+public class Task2 {
+    public static void main(String[] args) {
+
+        byte zero = 0;
+        short three = 3;
+        int one = 1;
+        float two = 2.0f;
+        char h = 'H';
+        boolean yes = true;
+
+        String output = "" + h + three + one + one + zero + " w" + zero + "rld " + two + " " + yes;
+
+        System.out.println(output);
+    }
+}
