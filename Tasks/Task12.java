5b3cf833ab8e23a642fc89b84e78de2d0f2bf6dc
julitobalagbagjr <302093482+julitobalagbagjr@users.noreply.github.com>
2026-08-21 03:57:16 +0800

    Task12

---
+++ Tasks/Task12.java
@@ -0,0 +1,29 @@
+public class Task12 {
+
+    static class Point {
+        private double x;
+        private double y;
+
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void print() {
+            System.out.println("(" + x + ", " + y + ")");
+        }
+
+        public void scale() {
+            x = x / 2;
+            y = y / 2;
+        }
+    }
+
+    public static void main(String[] args) {
+        Point p = new Point(32, 32);
+
+        p.print();
+        p.scale();
+        p.print();
+    }
+}
